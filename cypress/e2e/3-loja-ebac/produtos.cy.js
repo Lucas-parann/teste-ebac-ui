0c8be367051450eca1467eea9b226ee88c767d56
Lucas Paranhos <lucasvparranhos@outlook.com>
2025-07-29 17:33:24 -0300

    listas e produtos

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,16 @@
+///<references types="cypres"/>
+describe('funcionalidade: produtos ', () => {
+
+    beforeEach(() => {
+        cy.visit("http://lojaebac.ebaconline.art.br/produtos/")
+    });
+    it('Deve selecionar um produto da lista ', () => {
+        cy.get('.products > .row')
+       // .first()
+        //.last()
+        //.eq(2)
+        .contains('Aero Daily Fitness Tee')
+        .click()
+        cy.get('#tab-title-description > a').should('contain',('Descrição'))
+    });
+});     
