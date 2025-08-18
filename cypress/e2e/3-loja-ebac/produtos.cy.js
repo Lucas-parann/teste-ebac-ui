b6a20a6b7b51e15ea1f96dee6f2cd96470903066
Lucas Paranhos <lucasvparranhos@outlook.com>
2025-08-18 13:27:42 -0300

    mudança no gitgnore

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,16 +1,41 @@
 ///<references types="cypres"/>
+import produtosPage from "../../support/page-objetics/produtos.page";
 describe('funcionalidade: produtos ', () => {
 
     beforeEach(() => {
-        cy.visit("produtos")
+        produtosPage.visitarurl()
     });
     it('Deve selecionar um produto da lista ', () => {
-        cy.get('.products > .row')
-       // .first()
-        //.last()
-        //.eq(2)
-        .contains('Aero Daily Fitness Tee')
-        .click()
+      produtosPage.buscarProdutoLista('Aether Gym Pant')
         cy.get('#tab-title-description > a').should('contain',('Descrição'))
     });
+    it('Deve buscar um produto com sucesso ', () => {
+        let produto = 'Zeppelin Yoga Pant'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+
+    });
+    it('deve visitar a pagina do produto', () => {
+        produtosPage.visitarProduto("Zeppelin Yoga Pant")
+        cy.get('.product_title').should('contain', "Zeppelin Yoga Pant")
+    });
+    it('deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Ingrid Running Jacket')
+        produtosPage.addProdutoCarrinho('L','Red',qtd)
+         cy.get('.woocommerce-message').should('contain',qtd +' × “Ingrid Running Jacket” foram adicionados no seu carrinho.')
+    });
+      it.only('deve adicionar produto ao carrinho buscando da massa de dados ', () => {
+        cy.fixture('produtos').then(dados=>{
+      
+        produtosPage.buscarProduto(dados[0].nomeProduto)
+        produtosPage.addProdutoCarrinho(
+            dados[0].tamanho,
+            dados[0].cor,
+            dados[0].quantidade)
+        cy.get('.woocommerce-message').should('contain',dados[0].nomeProduto)
+        })
+  
+    });
 });     
+     
